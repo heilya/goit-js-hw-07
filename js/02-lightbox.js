c6277898941c65c4c0d4233fdfec0d4f8bb34892
heilya <123752915+heilya@users.noreply.github.com>
2023-06-12 14:22:21 +0200

    Зробила завдання 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,13 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const makeGallery = ({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+const markup = galleryItems.map((el) => makeGallery(el));
+gallery.insertAdjacentHTML("afterbegin", markup.join(""));
+
+const lightBox = new SimpleLightbox('.gallery a', {captionDelay:250, captionPosition:'bottom', captionsData:'alt'});
